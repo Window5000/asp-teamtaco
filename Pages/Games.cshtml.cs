bf02903a20e0a61b001eed461d0ac19232ea6859
Window5000 <84613801+Window5000@users.noreply.github.com>
2022-01-14 20:59:59 +0100

    Added files

---
+++ Pages/Games.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace asp.teamtaco.pages
+{
+    public class GamesModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
